ee16c4ddb31b5dc77bfe449112b7920aad311438
liguo <liguo@hujiang.com>
2020-03-22 13:49:37 +0800

    add unit test

--- hummer-test/src/main/java/com/hummer/redis/plugin/test/RedisTest.java
+++ hummer-test/src/main/java/com/hummer/redis/plugin/test/RedisTest.java
@@ -1,5 +1,6 @@
 package com.hummer.redis.plugin.test;
 
+
 import com.hummer.core.config.PropertiesConfig;
 import com.hummer.core.starter.BootStarterBean;
 import com.hummer.redis.plugin.RedisOp;
@@ -14,19 +15,16 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-/**
- * @Author: lee
- * @since:1.0.0
- * @Date: 2019/10/31 17:56
- **/
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class)
-@Import(value = {PropertiesConfig.class, BootStarterBean.class, RedisOp.class})
+@RunWith(value = SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ConfigFileApplicationContextInitializer.class)
+@Import(value = {PropertiesConfig.class
+        , BootStarterBean.class
+        , RedisOp.class
+        })
 @PropertySource(value = {"classpath:application.properties"})
 public class RedisTest {
+
     @Autowired
     private RedisOp redisOp;
 
@@ -35,28 +33,6 @@ public class RedisTest {
         redisOp.hash().hset("hummerHash1", "k1", "sssssssssssss");
         Assert.assertEquals("sssssssssssss"
                 , redisOp.hash().getByFieldKey("hummerHash1", "k1"));
-        //System.in.read();
     }
 
-    @Test
-    public void  set() throws IOException  {
-        Map<String,String> kv=new HashMap<>();
-        kv.put("setA1","test");
-        kv.put("setA2","test2");
-        redisOp.set().setMultipleStringByPipeline(kv,0,Boolean.FALSE);
-        System.in.read();
-    }
-
-    @Test
-    public void get() {
-        System.out.println("_________________________");
-        System.out.println(redisOp.set().getKey("LOCK_A"));
-    }
-
-
-    @Test
-    public void lock() {
-        Assert.assertEquals(Boolean.TRUE, redisOp.lock().lock("LOCK_A", 120));
-        Assert.assertEquals(Boolean.TRUE, redisOp.lock().freeLock("LOCK_A"));
-    }
 }
