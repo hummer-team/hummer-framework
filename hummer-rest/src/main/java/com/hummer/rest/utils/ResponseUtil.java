bfa371370978d80788e71f35f42b965d6b64abd1
liguo <liguo@panli.net>
2020-05-14 16:29:42 +0800

    add response parse util

--- hummer-rest/src/main/java/com/hummer/rest/utils/ResponseUtil.java
+++ hummer-rest/src/main/java/com/hummer/rest/utils/ResponseUtil.java
@@ -1,5 +1,10 @@
 package com.hummer.rest.utils;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.hummer.common.exceptions.AppException;
+import com.hummer.rest.model.ResourceResponse;
+
 /**
  * @Author: lee
  * @version:1.0.0
@@ -10,7 +15,37 @@ public class ResponseUtil {
 
     }
 
-    public static void response(){
+    public static <T> ResourceResponse<T> parseResponse(final String jsonValue
+            , TypeReference<ResourceResponse<T>> typeReference) {
+        return JSON.parseObject(jsonValue, typeReference);
+    }
 
+    public static <T> T parseResponseV2(final String jsonValue
+            , TypeReference<ResourceResponse<T>> typeReference) {
+        ResourceResponse<T> response = JSON.parseObject(jsonValue, typeReference);
+        return response != null ? response.getData() : null;
+    }
+    
+    public static <T> ResourceResponse<T> parseResponseWithStatus(final String jsonValue
+            , TypeReference<ResourceResponse<T>> typeReference) {
+        ResourceResponse<T> response = JSON.parseObject(jsonValue, typeReference);
+        if (response != null && response.getCode() != 0) {
+            throw new AppException(response.getCode(), response.getMessage());
+        }
+        return response;
+    }
+
+    public static <T> T parseResponseV2WithStatus(final String jsonValue
+            , TypeReference<ResourceResponse<T>> typeReference) {
+        ResourceResponse<T> response = JSON.parseObject(jsonValue, typeReference);
+
+        if (response == null) {
+            throw new AppException(50000, "call service failed.");
+        }
+
+        if (response.getCode() != 0) {
+            throw new AppException(response.getCode(), response.getMessage());
+        }
+        return response.getData();
     }
 }
