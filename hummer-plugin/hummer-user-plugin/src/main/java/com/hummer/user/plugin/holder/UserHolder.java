f1ac4e8530cfccd34c4e3ac08a8c86e6b3241da1
liguo <liguo@panli.net>
2020-06-24 15:24:40 +0800

    add user holder get user if null throw

--- hummer-plugin/hummer-user-plugin/src/main/java/com/hummer/user/plugin/holder/UserHolder.java
+++ hummer-plugin/hummer-user-plugin/src/main/java/com/hummer/user/plugin/holder/UserHolder.java
@@ -1,5 +1,6 @@
 package com.hummer.user.plugin.holder;
 
+import com.hummer.common.exceptions.AppException;
 import com.hummer.user.plugin.user.UserContext;
 
 import javax.validation.constraints.NotNull;
@@ -12,7 +13,12 @@ public class UserHolder {
     }
 
     public static UserContext get() {
-        return LOCAL.get();
+
+        UserContext context = LOCAL.get();
+        if (context == null) {
+            throw new AppException(50005, "this current user is null,please login");
+        }
+        return context;
     }
 
     public static String getTrueName() {
