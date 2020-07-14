31e9c29b9724105753b2b4bd26e9022578a9d476
chenwei <33944940+cw0722@users.noreply.github.com>
2020-07-14 11:46:20 +0800

    管理员查询修改

--- hummer-plugin/hummer-user-plugin/src/main/java/com/hummer/user/plugin/dto/request/UserBasicInfoPluginReqDto.java
+++ hummer-plugin/hummer-user-plugin/src/main/java/com/hummer/user/plugin/dto/request/UserBasicInfoPluginReqDto.java
@@ -10,9 +10,9 @@ import java.util.List;
  *
  * @author chen wei
  * @version 1.0
- *          <p>
- *          Copyright: Copyright (c) 2020
- *          </p>
+ * <p>
+ * Copyright: Copyright (c) 2020
+ * </p>
  * @date 2020/6/24 18:38
  */
 @Data
@@ -20,4 +20,7 @@ public class UserBasicInfoPluginReqDto {
 
     @ApiModelProperty("部门类型1、采购部,2、质检部,3、仓管部,4、客服部,5、财务部,6、风控部,7、市场运营部")
     private List<Integer> departmentTypes;
+
+    @ApiModelProperty("角色枚举10、采购部,20、质检部,30、仓管部,31、发货员,32、理货员,33、打包员,40、客服部,50、财务部,60、风控部,70、市场运营部")
+    private List<Integer> roleTypes;
 }
