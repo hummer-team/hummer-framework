049f4e526e6ee0693c8b52ad9ec217848c252046
chenwei <33944940+cw0722@users.noreply.github.com>
2020-09-12 14:28:07 +0800

    自定义线程池Bean名称枚举

---
+++ hummer-plugin/hummer-pipeline-plugin/src/main/java/com/hummer/pipeline/plugin/constant/Constants.java
@@ -0,0 +1,20 @@
+package com.hummer.pGipeline.plugin.constant;
+
+/**
+ * Constants
+ *
+ * @author chen wei
+ * @version 1.0
+ * <p>Copyright: Copyright (c) 2020</p>
+ * @date 2020/9/12 14:22
+ */
+public class Constants {
+
+    public static final String POOL_DEFAULT_TASK_GROUP = "defaultTaskGroup";
+
+    public static final String POOL_DEFAULT_TASK_GROUP_V2 = "defaultTaskGroupV2";
+
+    public static final String POOL_DEFAULT_TASK_OEN_THREAD_GROUP_V2 = "defaultTaskOenThreadGroupV2";
+
+    public static final String POOL_DEFAULT_TASK_GROUP_V3 = "defaultTaskGroupV3";
+}
