73118701f4b0a90460266ad18d1f118676ffb439
chenwei <33944940+cw0722@users.noreply.github.com>
2020-10-19 15:06:52 +0800

    mq model

---
+++ hummer-plugin/hummer-data-sync-plugin/src/main/java/com/hummer/data/sync/plugin/model/mq/MqPropertiesReqDto.java
@@ -0,0 +1,16 @@
+package com.hummer.data.sync.plugin.model.mq;
+
+import lombok.Data;
+
+/**
+ * MqPropertiesReqDto
+ *
+ * @author chen wei
+ * @version 1.0
+ * <p>Copyright: Copyright (c) 2020</p>
+ * @date 2020/9/22 14:49
+ */
+@Data
+public class MqPropertiesReqDto {
+
+}
---
+++ hummer-plugin/hummer-data-sync-plugin/src/main/java/com/hummer/data/sync/plugin/model/mq/ResourceMqConsumerReqDto.java
@@ -0,0 +1,40 @@
+package com.hummer.data.sync.plugin.model.mq;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+/**
+ * ResourceMqConsumerReqDto
+ *
+ * @author chen wei
+ * @version 1.0
+ * <p>Copyright: Copyright (c) 2020</p>
+ * @date 2020/9/22 14:26
+ */
+@Setter
+@Getter
+public class ResourceMqConsumerReqDto<T> {
+
+    @ApiModelProperty("businessId")
+    private String businessId;
+
+    @ApiModelProperty("业务数据body")
+    private byte[] bodys;
+
+    @ApiModelProperty("业务数据header")
+    private Map<String, String> head;
+
+    @ApiModelProperty("消息属性")
+    private MqPropertiesReqDto properties;
+
+
+    public T getBodys(TypeReference<T> reference) {
+
+        return JSON.parseObject(new String(bodys), reference);
+    }
+}
