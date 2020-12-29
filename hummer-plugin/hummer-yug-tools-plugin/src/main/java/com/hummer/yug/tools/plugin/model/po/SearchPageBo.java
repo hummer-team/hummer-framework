b632fcf9a954281db7cae9121de8020e0ccb4606
chenwei <33944940+cw0722@users.noreply.github.com>
2020-12-29 13:53:53 +0800

    检索基类

---
+++ hummer-plugin/hummer-yug-tools-plugin/src/main/java/com/hummer/yug/tools/plugin/model/po/SearchPageBo.java
@@ -0,0 +1,41 @@
+package com.hummer.yug.tools.plugin.model.po;
+
+import com.hummer.common.utils.ObjectCopyUtils;
+import com.hummer.rest.model.request.ResourcePageReqDto;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author chen wei
+ */
+@Getter
+@Setter
+public class SearchPageBo<T> {
+
+    private Integer pageNum;
+
+    private Integer pageSize;
+
+    private T data;
+
+    public Integer getCurrentIndex() {
+        if (pageNum == null || pageSize == null) {
+            return null;
+        }
+
+        return (pageNum - 1) * pageSize;
+    }
+
+
+    public static <R, T> SearchPageBo<T> builder(ResourcePageReqDto<R> reqDto, T t) {
+        SearchPageBo<T> pageBo = new SearchPageBo<>();
+        pageBo.setPageNum(reqDto.getPageNumber());
+        pageBo.setPageSize(reqDto.getPageSize());
+        if (reqDto.getQueryObject() != null) {
+            t = (T) ObjectCopyUtils.copy(reqDto.getQueryObject(), t.getClass());
+        }
+        pageBo.setData(t);
+        return pageBo;
+
+    }
+}
