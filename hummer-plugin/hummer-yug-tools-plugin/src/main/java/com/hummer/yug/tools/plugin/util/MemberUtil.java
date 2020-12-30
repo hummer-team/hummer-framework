7401f50800dbb6a20faa43b1f2ee30b694ad17fb
chenwei <33944940+cw0722@users.noreply.github.com>
2020-12-30 13:31:05 +0800

    用户操作方法

---
+++ hummer-plugin/hummer-yug-tools-plugin/src/main/java/com/hummer/yug/tools/plugin/util/MemberUtil.java
@@ -0,0 +1,47 @@
+package com.hummer.yug.tools.plugin.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * MemberUtil
+ *
+ * @author chen wei
+ * @version 1.0
+ * <p>Copyright: Copyright (c) 2020</p>
+ * @date 2020/12/30 11:49
+ */
+public class MemberUtil {
+
+    public static String composeCustomerId(@NotNull String sid) {
+
+        return sortString(3, sid);
+    }
+
+    public static String parseCustomerId(@NotNull String customerId) {
+
+        return sortString(3, customerId);
+    }
+
+    private static String sortString(String s) {
+        return sortString(0, s);
+    }
+
+    private static String sortString(int startIndex, String s) {
+        if (StringUtils.isEmpty(s)) {
+            return s;
+        }
+        char[] chars = s.toCharArray();
+        if (startIndex >= chars.length / 2) {
+            startIndex = 0;
+        }
+        char temp;
+        for (int i = startIndex; i <= chars.length / 2; i++) {
+            temp = chars[i];
+            chars[i] = chars[chars.length - i - 1];
+            chars[chars.length - i - 1] = temp;
+        }
+        return String.valueOf(chars);
+    }
+}
--- hummer-plugin/hummer-yug-user-plugin/src/main/java/com/hummer/yug/user/plugin/holder/UserHolder.java
+++ hummer-plugin/hummer-yug-user-plugin/src/main/java/com/hummer/yug/user/plugin/holder/UserHolder.java
@@ -4,6 +4,7 @@ import com.hummer.common.exceptions.AppException;
 import com.hummer.common.utils.AppBusinessAssert;
 import com.hummer.common.utils.CommonUtil;
 import com.hummer.yug.tools.plugin.constants.Constants;
+import com.hummer.yug.tools.plugin.util.MemberUtil;
 import com.hummer.yug.tools.plugin.util.SysEnums;
 import com.hummer.yug.user.plugin.dto.response.ShopInfoRespDto;
 import com.hummer.yug.user.plugin.user.UserContext;
@@ -33,6 +34,11 @@ public class UserHolder {
     }
 
 
+    public static String getCustomerIdNotNull() {
+        
+        return MemberUtil.composeCustomerId(get().getYgfUserCode());
+    }
+
     public static void set(@NotNull UserContext userContext) {
         LOCAL.set(userContext);
     }
@@ -60,7 +66,6 @@ public class UserHolder {
 
         return CommonUtil.typeChange(RequestContextHolder.get(Constants.HeadersKey.OPERATOR_ID), Long.class);
     }
-
 
     public static Long getOperatorIdAssertNull() {
 
